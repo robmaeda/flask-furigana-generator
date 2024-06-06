8b30f762fdf82d9a9d45ce6352b454c2ebf90651
Robert Maeda <rob.maeda3@gmail.com>
2024-06-05 22:19:49 -0700

    Change to pykakasi

--- api/index.py
+++ api/index.py
@@ -1,25 +1,19 @@
 from flask import Flask, request, jsonify
-import MeCab
-import jaconv
+import pykakasi
 
 app = Flask(__name__)
-tagger = MeCab.Tagger("-Ochasen")
+kks = pykakasi.kakasi()
 
 def process_text(text):
-    parsed = tagger.parse(text)
     output = ""
-    lines = parsed.split('\n')
-    for line in lines:
-        columns = line.split('\t')
-        if len(columns) >= 3:
-            original = columns[0]
-            reading = columns[1]
-            hiragana = jaconv.kata2hira(reading)
-            if original != hiragana:
-                # Create Ruby annotation if original and reading are different
-                output += f"<ruby>{original}<rt>{hiragana}</rt></ruby>"
-            else:
-                output += original
+    converted_text = kks.convert(text)
+    for phrase in converted_text:
+        original = phrase["orig"]
+        hiragana = phrase["hira"]
+        if original != hiragana:
+            output += f"<ruby>{original}<rt>{hiragana}</rt></ruby>"
+        else:
+            output += original
     return output
 
 @app.route('/furigana', methods=['POST'])
@@ -27,6 +21,7 @@ def furigana():
     if request.is_json:
         text = request.json['text']
         result = process_text(text)
+        print(result)
         return jsonify({'result': result})
     else:
         return "Invalid or missing JSON", 400
