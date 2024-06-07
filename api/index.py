a60f59e0fc119f880fa6fa6eb9e5f91af0ba92c7
Robert Maeda <rob.maeda3@gmail.com>
2024-06-07 11:25:57 -0700

    Remove backslash

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ def process_text(text):
             output += token
     return output
 
-CORS(app, resources={r"/api/*": {"origins": "https://react-furigana-generator.vercel.app/"}})
+CORS(app, resources={r"/api/*": {"origins": "https://react-furigana-generator.vercel.app"}})
 @app.route('/furigana', methods=['POST'])
 def furigana():
     if request.is_json:
